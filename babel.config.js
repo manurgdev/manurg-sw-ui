a6f01aceb4dc2727c11cb1c367ca57a18ec50c95
manurgdev <manuelrodriguezgil@informaticos.com>
2021-08-04 10:21:55 +0200

    Fixed babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     "presets": [
         "next/babel"
+    ],
+    "plugins": [
+        [
+            "babel-plugin-styled-components",
+            {
+                "ssr": true,
+                "displayName": true,
+                "preprocess": false
+            }
+        ]
     ]
 }
